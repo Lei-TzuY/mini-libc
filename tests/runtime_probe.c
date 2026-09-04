cf0e645ed70253223cdff717bb08306a71b79eb3
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-05 07:06:54 +0800

    test: exercise normal and immediate termination paths

--- tests/runtime_probe.c
+++ tests/runtime_probe.c
@@ -1,4 +1,5 @@
 #include <mini/syscall.h>
+#include <stdlib.h>
 
 static int equals(const char *left, const char *right)
 {
@@ -9,11 +10,88 @@ static int equals(const char *left, const char *right)
     return *left == *right;
 }
 
+static void write_marker(char marker)
+{
+    if (mini_sys_write(1, &marker, 1) != 1) {
+        _Exit(90);
+    }
+}
+
+static void marker_a(void)
+{
+    write_marker('A');
+}
+
+static void marker_b(void)
+{
+    write_marker('B');
+}
+
+static void marker_c(void)
+{
+    write_marker('C');
+}
+
+static void marker_x(void)
+{
+    write_marker('X');
+}
+
+static void register_three(void)
+{
+    if (atexit(marker_a) != 0 || atexit(marker_b) != 0 || atexit(marker_c) != 0) {
+        _Exit(91);
+    }
+}
+
+static int run_termination_probe(const char *mode)
+{
+    unsigned int i;
+
+    if (equals(mode, "return-exit")) {
+        register_three();
+        return 23;
+    }
+
+    if (equals(mode, "call-exit")) {
+        register_three();
+        exit(24);
+    }
+
+    if (equals(mode, "quick-exit")) {
+        register_three();
+        _Exit(25);
+    }
+
+    if (equals(mode, "capacity")) {
+        for (i = 0; i < 32U; ++i) {
+            if (atexit(marker_x) != 0) {
+                _Exit(92);
+            }
+        }
+        if (atexit(marker_x) == 0) {
+            _Exit(93);
+        }
+        return 26;
+    }
+
+    return -1;
+}
+
 int main(int argc, char **argv, char **envp)
 {
     static const char ok[] = "runtime-ok\n";
     int saw_sentinel = 0;
     char **entry;
+
+    if (argc == 2 && equals(argv[0], "./build/runtime_probe")) {
+        int status = run_termination_probe(argv[1]);
+
+        if (status >= 0) {
+            return status;
+        }
+        return 13;
+    }
 
     if (argc != 3 || !equals(argv[0], "./build/runtime_probe") ||
         !equals(argv[1], "alpha") || !equals(argv[2], "beta") ||
