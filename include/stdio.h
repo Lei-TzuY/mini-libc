fe37977fc737d60c4d92f5cc030ff503d1c25399
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-05 07:16:32 +0800

    feat: expose standard stream interface

--- include/stdio.h
+++ include/stdio.h
@@ -3,7 +3,22 @@
 
 #define EOF (-1)
 
+typedef struct __mini_FILE FILE;
+
+extern FILE *stdin;
+extern FILE *stdout;
+extern FILE *stderr;
+
+int fgetc(FILE *stream);
+int getc(FILE *stream);
+int getchar(void);
+int fputc(int c, FILE *stream);
+int putc(int c, FILE *stream);
 int putchar(int c);
+int fputs(const char *restrict s, FILE *restrict stream);
 int puts(const char *s);
+int feof(FILE *stream);
+int ferror(FILE *stream);
+void clearerr(FILE *stream);
 
 #endif
