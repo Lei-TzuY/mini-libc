b0d020384e1e4f6e77b20ae101924b36b546f4a7
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-05 16:11:32 +0800

    Add C11 thread runtime surface

---
+++ include/threads.h
@@ -0,0 +1,35 @@
+#ifndef MINI_LIBC_THREADS_H
+#define MINI_LIBC_THREADS_H
+
+typedef unsigned long thrd_t;
+typedef int (*thrd_start_t)(void *);
+
+typedef struct {
+    int __state;
+} mtx_t;
+
+enum {
+    thrd_success = 0,
+    thrd_nomem = 1,
+    thrd_timedout = 2,
+    thrd_busy = 3,
+    thrd_error = 4
+};
+
+enum {
+    mtx_plain = 0
+};
+
+int thrd_create(thrd_t *thr, thrd_start_t func, void *arg);
+int thrd_join(thrd_t thr, int *res);
+thrd_t thrd_current(void);
+int thrd_equal(thrd_t lhs, thrd_t rhs);
+_Noreturn void thrd_exit(int res);
+
+int mtx_init(mtx_t *mtx, int type);
+int mtx_lock(mtx_t *mtx);
+int mtx_trylock(mtx_t *mtx);
+int mtx_unlock(mtx_t *mtx);
+void mtx_destroy(mtx_t *mtx);
+
+#endif
