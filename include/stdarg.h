d0e461ba732bee800970446b8199e7e991d0a47e
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-05 10:20:44 +0800

    fix: align tiny-c va_list call ABI

--- include/stdarg.h
+++ include/stdarg.h
@@ -9,15 +9,15 @@ typedef __builtin_va_list va_list;
 #define __va_copy(dest, src) va_copy((dest), (src))
 #define va_end(ap) __builtin_va_end((ap))
 #else
-typedef struct __mini_va_list {
+typedef struct __mini_va_list_tag {
     unsigned int gp_offset;
     unsigned int fp_offset;
     void *overflow_arg_area;
     void *reg_save_area;
-} va_list;
-#define va_start(ap, last) __builtin_va_start(&(ap))
-#define va_arg(ap, type) __builtin_va_arg(&(ap), type)
-#define va_copy(dest, src) ((dest) = (src))
+} va_list[1];
+#define va_start(ap, last) __builtin_va_start((ap))
+#define va_arg(ap, type) __builtin_va_arg((ap), type)
+#define va_copy(dest, src) ((dest)[0] = (src)[0])
 #define __va_copy(dest, src) va_copy((dest), (src))
 #define va_end(ap) ((void)0)
 #endif
