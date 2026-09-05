a6a793e6898dbd819a170991de4db7f68aa7cd08
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-05 17:59:10 +0800

    Wire condition variable runtime and tests

--- mk/threads.mk
+++ mk/threads.mk
@@ -1,5 +1,9 @@
-$(LIBC): $(BUILD)/atomic.o $(BUILD)/thread_runtime.o $(BUILD)/thread.o $(BUILD)/thread_entry.o
-all: $(BUILD)/thread_probe $(BUILD)/thread_exit_group_probe
+CONDITION_RENAMES := -Dmini_sys_futex=mini_test_futex \
+                     -Dmtx_lock=mini_test_mtx_lock \
+                     -Dmtx_unlock=mini_test_mtx_unlock
+
+$(LIBC): $(BUILD)/atomic.o $(BUILD)/condition.o $(BUILD)/thread_runtime.o $(BUILD)/thread.o $(BUILD)/thread_entry.o
+all: $(BUILD)/thread_probe $(BUILD)/thread_exit_group_probe $(BUILD)/condition_probe $(BUILD)/condition_test
 inspect: thread_inspect
 test: thread_test_run
 
@@ -7,6 +11,9 @@ test: thread_test_run
 
 $(BUILD)/atomic.o: src/internal/atomic.S | $(BUILD)
 	$(CC) $(ASFLAGS) -c $< -o $@
+
+$(BUILD)/condition.o: src/thread/condition.c include/threads.h include/errno.h include/mini/syscall.h | $(BUILD)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@
 
 $(BUILD)/thread_runtime.o: src/thread/runtime.c src/internal/thread_runtime.h include/mini/syscall.h | $(BUILD)
 	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@
@@ -29,10 +36,27 @@ $(BUILD)/thread_exit_group_probe.o: tests/thread_exit_group_probe.c include/thre
 $(BUILD)/thread_exit_group_probe: $(BUILD)/thread_exit_group_probe.o $(CRT0) $(LIBC)
 	$(LD) -static -e _start --build-id=none -o $@ $(BUILD)/thread_exit_group_probe.o $(CRT0) $(LIBC)
 
-thread_test_run: $(BUILD)/thread_probe $(BUILD)/thread_exit_group_probe
+$(BUILD)/condition_probe.o: tests/condition_probe.c include/threads.h include/errno.h include/mini/syscall.h | $(BUILD)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@
+
+$(BUILD)/condition_probe: $(BUILD)/condition_probe.o $(CRT0) $(LIBC)
+	$(LD) -static -e _start --build-id=none -o $@ $(BUILD)/condition_probe.o $(CRT0) $(LIBC)
+
+$(BUILD)/condition_test_impl.o: src/thread/condition.c include/threads.h include/errno.h include/mini/syscall.h | $(BUILD)
+	$(CC) $(CPPFLAGS) $(CFLAGS) $(CONDITION_RENAMES) -c $< -o $@
+
+$(BUILD)/condition_test.o: tests/condition_test.c include/threads.h include/errno.h include/mini/syscall.h | $(BUILD)
+	$(CC) $(CPPFLAGS) $(HOST_CFLAGS) -c $< -o $@
+
+$(BUILD)/condition_test: $(BUILD)/condition_test.o $(BUILD)/condition_test_impl.o $(BUILD)/errno.o
+	$(CC) $(HOST_LDFLAGS) -o $@ $^
+
+thread_test_run: $(BUILD)/thread_probe $(BUILD)/thread_exit_group_probe $(BUILD)/condition_probe $(BUILD)/condition_test
 	@test "$$($(BUILD)/thread_probe)" = "threads-ok"
 	@output="$$($(BUILD)/thread_exit_group_probe)"; status=$$?; \
 		test "$$status" -eq 37 && test -z "$$output"
+	@test "$$($(BUILD)/condition_probe)" = "conditions-ok"
+	@$(BUILD)/condition_test
 
-thread_inspect: $(BUILD)/thread_probe $(BUILD)/thread_exit_group_probe
-	./tests/verify-no-host-libc.sh $(BUILD)/thread_probe $(BUILD)/thread_exit_group_probe
+thread_inspect: $(BUILD)/thread_probe $(BUILD)/thread_exit_group_probe $(BUILD)/condition_probe
+	./tests/verify-no-host-libc.sh $(BUILD)/thread_probe $(BUILD)/thread_exit_group_probe $(BUILD)/condition_probe
