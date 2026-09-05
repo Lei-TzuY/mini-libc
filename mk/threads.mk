f91ba0eddccd3dd5837b4ad1ba862a8baa9b32b1
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-05 16:28:29 +0800

    Gate process-wide thread termination

--- mk/threads.mk
+++ mk/threads.mk
@@ -1,5 +1,5 @@
 $(LIBC): $(BUILD)/thread_runtime.o $(BUILD)/thread.o $(BUILD)/thread_entry.o
-all: $(BUILD)/thread_probe
+all: $(BUILD)/thread_probe $(BUILD)/thread_exit_group_probe
 inspect: thread_inspect
 test: thread_test_run
 
@@ -20,8 +20,16 @@ $(BUILD)/thread_probe.o: tests/thread_probe.c include/threads.h include/errno.h 
 $(BUILD)/thread_probe: $(BUILD)/thread_probe.o $(CRT0) $(LIBC)
 	$(LD) -static -e _start --build-id=none -o $@ $(BUILD)/thread_probe.o $(CRT0) $(LIBC)
 
-thread_test_run: $(BUILD)/thread_probe
-	@test "$$($(BUILD)/thread_probe)" = "threads-ok"
+$(BUILD)/thread_exit_group_probe.o: tests/thread_exit_group_probe.c include/threads.h include/stdlib.h include/mini/syscall.h | $(BUILD)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@
 
-thread_inspect: $(BUILD)/thread_probe
-	./tests/verify-no-host-libc.sh $(BUILD)/thread_probe
+$(BUILD)/thread_exit_group_probe: $(BUILD)/thread_exit_group_probe.o $(CRT0) $(LIBC)
+	$(LD) -static -e _start --build-id=none -o $@ $(BUILD)/thread_exit_group_probe.o $(CRT0) $(LIBC)
+
+thread_test_run: $(BUILD)/thread_probe $(BUILD)/thread_exit_group_probe
+	@test "$$($(BUILD)/thread_probe)" = "threads-ok"
+	@output="$$($(BUILD)/thread_exit_group_probe)"; status=$$?; \
+		test "$$status" -eq 37 && test -z "$$output"
+
+thread_inspect: $(BUILD)/thread_probe $(BUILD)/thread_exit_group_probe
+	./tests/verify-no-host-libc.sh $(BUILD)/thread_probe $(BUILD)/thread_exit_group_probe
