74eff54166d3ced721ac7948709f15ccf1d505ec
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-05 10:32:49 +0800

    fix: pass tiny-c va_list object addresses

--- include/stdarg.h
+++ include/stdarg.h
@@ -15,8 +15,8 @@ typedef struct __mini_va_list_tag {
     void *overflow_arg_area;
     void *reg_save_area;
 } va_list[1];
-#define va_start(ap, last) __builtin_va_start((ap))
-#define va_arg(ap, type) __builtin_va_arg((ap), type)
+#define va_start(ap, last) __builtin_va_start(&(ap)[0])
+#define va_arg(ap, type) __builtin_va_arg(&(ap)[0], type)
 #define va_copy(dest, src) ((dest)[0] = (src)[0])
 #define __va_copy(dest, src) va_copy((dest), (src))
 #define va_end(ap) ((void)0)
