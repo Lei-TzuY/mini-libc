afc5d4216f584ea3ee37d9eb699e89c728890e26
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-05 21:06:00 +0800

    Wire timed blocking runtime tests

--- mk/threads.mk
+++ mk/threads.mk
@@ -1,8 +1,9 @@
 CONDITION_RENAMES := -Dmini_sys_futex=mini_test_futex \
                      -Dmtx_lock=mini_test_mtx_lock \
                      -Dmtx_unlock=mini_test_mtx_unlock
+SLEEP_RENAMES := -Dmini_sys_nanosleep=mini_test_nanosleep
 
-$(LIBC): $(BUILD)/atomic.o $(BUILD)/condition.o $(BUILD)/thread_runtime.o $(BUILD)/thread.o $(BUILD)/thread_entry.o
+$(LIBC): $(BUILD)/atomic.o $(BUILD)/condition.o $(BUILD)/sleep.o $(BUILD)/thread_runtime.o $(BUILD)/thread.o $(BUILD)/thread_entry.o
 all: $(BUILD)/thread_probe $(BUILD)/thread_exit_group_probe $(BUILD)/condition_probe $(BUILD)/condition_test
 inspect: thread_inspect
 test: thread_test_run
@@ -12,7 +13,10 @@ test: thread_test_run
 $(BUILD)/atomic.o: src/internal/atomic.S | $(BUILD)
 	$(CC) $(ASFLAGS) -c $< -o $@
 
-$(BUILD)/condition.o: src/thread/condition.c include/threads.h include/errno.h include/mini/syscall.h | $(BUILD)
+$(BUILD)/condition.o: src/thread/condition.c include/threads.h include/time.h include/errno.h include/mini/syscall.h | $(BUILD)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@
+
+$(BUILD)/sleep.o: src/thread/sleep.c include/threads.h include/time.h include/errno.h include/mini/syscall.h | $(BUILD)
 	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@
 
 $(BUILD)/thread_runtime.o: src/thread/runtime.c src/internal/thread_runtime.h include/mini/syscall.h | $(BUILD)
@@ -36,19 +40,22 @@ $(BUILD)/thread_exit_group_probe.o: tests/thread_exit_group_probe.c include/thre
 $(BUILD)/thread_exit_group_probe: $(BUILD)/thread_exit_group_probe.o $(CRT0) $(LIBC)
 	$(LD) -static -e _start --build-id=none -o $@ $(BUILD)/thread_exit_group_probe.o $(CRT0) $(LIBC)
 
-$(BUILD)/condition_probe.o: tests/condition_probe.c include/threads.h include/errno.h include/mini/syscall.h | $(BUILD)
+$(BUILD)/condition_probe.o: tests/condition_probe.c include/threads.h include/time.h include/errno.h include/mini/syscall.h | $(BUILD)
 	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@
 
 $(BUILD)/condition_probe: $(BUILD)/condition_probe.o $(CRT0) $(LIBC)
 	$(LD) -static -e _start --build-id=none -o $@ $(BUILD)/condition_probe.o $(CRT0) $(LIBC)
 
-$(BUILD)/condition_test_impl.o: src/thread/condition.c include/threads.h include/errno.h include/mini/syscall.h | $(BUILD)
+$(BUILD)/condition_test_impl.o: src/thread/condition.c include/threads.h include/time.h include/errno.h include/mini/syscall.h | $(BUILD)
 	$(CC) $(CPPFLAGS) $(CFLAGS) $(CONDITION_RENAMES) -c $< -o $@
 
-$(BUILD)/condition_test.o: tests/condition_test.c include/threads.h include/errno.h include/mini/syscall.h | $(BUILD)
+$(BUILD)/sleep_test_impl.o: src/thread/sleep.c include/threads.h include/time.h include/errno.h include/mini/syscall.h | $(BUILD)
+	$(CC) $(CPPFLAGS) $(CFLAGS) $(SLEEP_RENAMES) -c $< -o $@
+
+$(BUILD)/condition_test.o: tests/condition_test.c include/threads.h include/time.h include/errno.h include/mini/syscall.h | $(BUILD)
 	$(CC) $(CPPFLAGS) $(HOST_CFLAGS) -c $< -o $@
 
-$(BUILD)/condition_test: $(BUILD)/condition_test.o $(BUILD)/condition_test_impl.o $(BUILD)/errno.o
+$(BUILD)/condition_test: $(BUILD)/condition_test.o $(BUILD)/condition_test_impl.o $(BUILD)/sleep_test_impl.o $(BUILD)/errno.o
 	$(CC) $(HOST_LDFLAGS) -o $@ $^
 
 thread_test_run: $(BUILD)/thread_probe $(BUILD)/thread_exit_group_probe $(BUILD)/condition_probe $(BUILD)/condition_test
