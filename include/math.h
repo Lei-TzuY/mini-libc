737fde888f61e8c626079ecbb5cc24d2d7fee3bb
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-07 13:38:41 +0800

    Add basic math public surface

---
+++ include/math.h
@@ -0,0 +1,30 @@
+#ifndef MINI_LIBC_MATH_H
+#define MINI_LIBC_MATH_H
+
+#define MATH_ERRNO 1
+#define MATH_ERREXCEPT 2
+#define math_errhandling MATH_ERRNO
+
+double fabs(double x);
+float fabsf(float x);
+double copysign(double x, double y);
+float copysignf(float x, float y);
+
+double fmin(double x, double y);
+float fminf(float x, float y);
+double fmax(double x, double y);
+float fmaxf(float x, float y);
+
+double trunc(double x);
+float truncf(float x);
+double floor(double x);
+float floorf(float x);
+double ceil(double x);
+float ceilf(float x);
+double round(double x);
+float roundf(float x);
+
+double sqrt(double x);
+float sqrtf(float x);
+
+#endif
