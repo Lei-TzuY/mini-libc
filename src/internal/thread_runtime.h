074b29351b2d7421d01965d4677db65cc3da1f36
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-07 08:11:31 +0800

    Add bounded native TLS storage to TCB

--- src/internal/thread_runtime.h
+++ src/internal/thread_runtime.h
@@ -2,8 +2,12 @@
 #define MINI_LIBC_INTERNAL_THREAD_RUNTIME_H
 
 #define MINI_TSS_MAX_KEYS 32U
+#define MINI_NATIVE_TLS_STORAGE_SIZE 4096U
+#define MINI_NATIVE_TLS_MAX_ALIGNMENT 16U
 
 struct mini_thread_tcb {
+    _Alignas(MINI_NATIVE_TLS_MAX_ALIGNMENT)
+        unsigned char native_tls[MINI_NATIVE_TLS_STORAGE_SIZE];
     struct mini_thread_tcb *self;
     void *control;
     int errno_value;
@@ -12,7 +16,8 @@ struct mini_thread_tcb {
     unsigned int tss_generations[MINI_TSS_MAX_KEYS];
 };
 
-void __mini_thread_runtime_init_main(void);
+void __mini_thread_runtime_init_main(long *initial_stack);
+void *__mini_thread_prepare_tcb(struct mini_thread_tcb *tcb);
 struct mini_thread_tcb *__mini_thread_current_tcb(void);
 void __mini_errno_set_provider(int *(*provider)(void));
 void __mini_tss_run_destructors(void);
