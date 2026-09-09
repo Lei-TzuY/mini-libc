eb952f448c3792f94d758d8c357f38e7ce2e264b
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-09 19:14:02 +0800

    Keep standalone buffering gate orientation-aware

--- tests/verify-buffering.sh
+++ tests/verify-buffering.sh
@@ -11,6 +11,8 @@ mkdir -p "$OUT"
 $CC -Iinclude $CFLAGS \
     -Dmini_sys_read=mini_test_read -Dmini_sys_write=mini_test_write \
     -c src/stdio/stdio.c -o "$OUT/stdio.o"
+$CC -Iinclude $CFLAGS -c src/stdio/orientation.c -o "$OUT/orientation.o"
+$CC -Iinclude $CFLAGS -c src/stdio/byte_sync.c -o "$OUT/byte_sync.o"
 $CC -Iinclude $CFLAGS \
     -Dmini_sys_openat=mini_test_openat -Dmini_sys_close=mini_test_close \
     -Dmalloc=mini_test_malloc -Dfree=mini_test_free \
@@ -20,7 +22,9 @@ $CC -Iinclude $CFLAGS -c src/stdio/position.c -o "$OUT/position.o"
 $CC -Iinclude $CFLAGS -c tests/stdio_buffering_test.c -o "$OUT/test.o"
 $CC -Iinclude $CFLAGS -c src/errno/errno.c -o "$OUT/errno.o"
 $CC $CFLAGS -c tests/stdio_lock_fake.c -o "$OUT/stdio_lock_fake.o"
-$CC -no-pie -o "$OUT/test" "$OUT/test.o" "$OUT/stdio.o" "$OUT/file.o" \
-    "$OUT/file_sync.o" "$OUT/position.o" "$OUT/errno.o" "$OUT/stdio_lock_fake.o"
+$CC -no-pie -o "$OUT/test" "$OUT/test.o" "$OUT/stdio.o" \
+    "$OUT/orientation.o" "$OUT/byte_sync.o" "$OUT/file.o" \
+    "$OUT/file_sync.o" "$OUT/position.o" "$OUT/errno.o" \
+    "$OUT/stdio_lock_fake.o"
 
 "$OUT/test"
