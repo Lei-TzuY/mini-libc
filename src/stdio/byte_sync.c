bdb9cd5db92e53534daf8456f21e41d126904e60
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-09 19:08:45 +0800

    Split byte stdio orientation wrappers

--- src/stdio/byte_sync.c
+++ src/stdio/byte_sync.c
@@ -1,13 +1,8 @@
-#include <stddef.h>
 #include <stdio.h>
 
 #define MINI_STDIO_BYTE_PUBLIC_WRAPPER 1
 #include "stdio_internal.h"
 
-size_t __mini_fread_byte_core(void *restrict ptr, size_t size, size_t nmemb,
-                              FILE *restrict stream);
-size_t __mini_fwrite_byte_core(const void *restrict ptr, size_t size,
-                               size_t nmemb, FILE *restrict stream);
 int __mini_fgetc_byte_core(FILE *stream);
 int __mini_getc_byte_core(FILE *stream);
 int __mini_getchar_byte_core(void);
@@ -18,36 +13,6 @@ int __mini_putc_byte_core(int c, FILE *stream);
 int __mini_putchar_byte_core(int c);
 int __mini_fputs_byte_core(const char *restrict s, FILE *restrict stream);
 int __mini_puts_byte_core(const char *s);
-
-size_t fread(void *restrict ptr, size_t size, size_t nmemb,
-             FILE *restrict stream)
-{
-    size_t result;
-
-    __mini_stdio_lock();
-    if (__mini_stdio_require_byte(stream) == EOF) {
-        result = 0U;
-    } else {
-        result = __mini_fread_byte_core(ptr, size, nmemb, stream);
-    }
-    __mini_stdio_unlock();
-    return result;
-}
-
-size_t fwrite(const void *restrict ptr, size_t size, size_t nmemb,
-              FILE *restrict stream)
-{
-    size_t result;
-
-    __mini_stdio_lock();
-    if (__mini_stdio_require_byte(stream) == EOF) {
-        result = 0U;
-    } else {
-        result = __mini_fwrite_byte_core(ptr, size, nmemb, stream);
-    }
-    __mini_stdio_unlock();
-    return result;
-}
 
 int fgetc(FILE *stream)
 {
