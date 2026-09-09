6c826f60edf5f1caf05df20a1405ef827f5c7b20
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-09 20:07:52 +0800

    Wire wide formatted output into locale runtime

--- mk/locale.mk
+++ mk/locale.mk
@@ -8,7 +8,7 @@ MULTIBYTE_RENAMES := -Dmblen=mini_test_mblen -Dmbtowc=mini_test_mbtowc \
                      -Dwcstombs=mini_test_wcstombs
 
 $(LIBC): $(BUILD)/locale.o $(BUILD)/wchar.o $(BUILD)/wide_stdio.o \
-         $(BUILD)/multibyte.o
+         $(BUILD)/wide_format.o $(BUILD)/multibyte.o
 all: $(BUILD)/locale_probe $(BUILD)/locale_differential \
      $(BUILD)/wchar_probe $(BUILD)/wchar_differential \
      $(BUILD)/wide_stdio_probe
@@ -25,6 +25,11 @@ $(BUILD)/wchar.o: src/wchar/wchar.c include/wchar.h include/stddef.h include/err
 
 $(BUILD)/wide_stdio.o: src/wchar/wide_stdio.c include/wchar.h include/stdio.h \
                        src/stdio/stdio_internal.h include/errno.h | $(BUILD)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@
+
+$(BUILD)/wide_format.o: src/wchar/wide_format.c include/wchar.h include/stdio.h \
+                        include/stdarg.h include/stdlib.h include/errno.h \
+                        src/stdio/stdio_internal.h | $(BUILD)
 	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@
 
 $(BUILD)/multibyte.o: src/stdlib/multibyte.c include/stdlib.h include/wchar.h include/stddef.h | $(BUILD)
