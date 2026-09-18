1338ab29455907b62351450aa5681abeea1a4d90
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-18 14:53:01 +0800

    Integrate UTF-8 wide stream characters

--- src/wchar/wide_stdio.c
+++ src/wchar/wide_stdio.c
@@ -6,6 +6,7 @@
 #define MINI_STDIO_BYTE_PUBLIC_WRAPPER 1
 #include "../stdio/stdio_internal.h"
 #include "wide_internal.h"
+#include "wchar_internal.h"
 
 static wint_t wide_error(FILE *stream, int error)
 {
@@ -43,33 +44,44 @@ static int wide_require_writable(FILE *stream)
 
 wint_t __mini_wide_read_character_unlocked(FILE *stream)
 {
-    unsigned char byte;
     wchar_t wc = 0;
     mbstate_t state = {0U, 0U};
-    size_t converted;
+    int utf8 = (stream->state & MINI_FILE_WIDE_UTF8) != 0U;
 
-    if (__mini_stdio_read(stream, &byte, 1U) != 1U) {
-        return WEOF;
-    }
+    for (;;) {
+        unsigned char byte;
+        size_t converted;
 
-    converted = mbrtowc(&wc, (const char *)&byte, 1U, &state);
-    if (converted == (size_t)-1) {
-        return wide_error(stream, errno);
+        if (__mini_stdio_read(stream, &byte, 1U) != 1U) {
+            if (!mbsinit(&state)) {
+                return wide_error(stream, EILSEQ);
+            }
+            return WEOF;
+        }
+
+        converted = __mini_mbrtowc_mode(&wc, (const char *)&byte, 1U,
+                                        &state, utf8);
+        if (converted == (size_t)-1) {
+            return wide_error(stream, errno);
+        }
+        if (converted != (size_t)-2) {
+            return (wint_t)wc;
+        }
     }
-    return (wint_t)wc;
 }
 
 static wint_t wide_write_character_unlocked(wchar_t wc, FILE *stream)
 {
-    char byte;
+    char bytes[4];
     mbstate_t state = {0U, 0U};
     size_t converted;
+    int utf8 = (stream->state & MINI_FILE_WIDE_UTF8) != 0U;
 
-    converted = wcrtomb(&byte, wc, &state);
+    converted = __mini_wcrtomb_mode(bytes, wc, &state, utf8);
     if (converted == (size_t)-1) {
         return wide_error(stream, errno);
     }
-    if (__mini_stdio_write(stream, (const unsigned char *)&byte, converted) !=
+    if (__mini_stdio_write(stream, (const unsigned char *)bytes, converted) !=
         converted) {
         return WEOF;
     }
@@ -78,26 +90,24 @@ static wint_t wide_write_character_unlocked(wchar_t wc, FILE *stream)
 
 wint_t __mini_wide_unget_character_unlocked(wint_t wc, FILE *stream)
 {
-    char byte;
+    char bytes[4];
     mbstate_t state = {0U, 0U};
     size_t converted;
+    int utf8;
 
-    if (wc == WEOF || stream == (FILE *)0 ||
-        (stream->mode & MINI_FILE_READABLE) == 0U ||
-        (stream->state & MINI_FILE_WRITE_NEEDS_SYNC) != 0U ||
-        stream->pushback_valid != 0U) {
+    if (wc == WEOF || stream == (FILE *)0) {
         return wide_error(stream, EINVAL);
     }
 
-    converted = wcrtomb(&byte, (wchar_t)wc, &state);
-    if (converted == (size_t)-1 || converted != 1U) {
-        return wide_error(stream, converted == (size_t)-1 ? errno : EILSEQ);
+    utf8 = (stream->state & MINI_FILE_WIDE_UTF8) != 0U;
+    converted = __mini_wcrtomb_mode(bytes, (wchar_t)wc, &state, utf8);
+    if (converted == (size_t)-1) {
+        return wide_error(stream, errno);
     }
-
-    stream->pushback_byte = (unsigned char)byte;
-    stream->pushback_valid = 1U;
-    stream->state &= ~MINI_FILE_EOF;
-    stream->state |= MINI_FILE_READ_NEEDS_POSITION;
+    if (__mini_stdio_pushback_unlocked(stream,
+            (const unsigned char *)bytes, converted) == EOF) {
+        return WEOF;
+    }
     return wc;
 }
 
