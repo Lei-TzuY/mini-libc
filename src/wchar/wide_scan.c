1c58ec48b535d6cd03ea82d3d1391d3ed0b83dd4
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-18 19:13:06 +0800

    feat: encode wide scan formats by locale mode

--- src/wchar/wide_scan.c
+++ src/wchar/wide_scan.c
@@ -6,35 +6,63 @@
 #include <wchar.h>
 
 #define MINI_STDIO_BYTE_PUBLIC_WRAPPER 1
+#include "../locale/locale_internal.h"
 #include "../stdio/stdio_internal.h"
+#include "wchar_internal.h"
 
 int __mini_vwscan_dispatch(FILE *stream, const char *format, va_list ap);
 int __mini_vwsscan_dispatch(const wchar_t *input, const char *format, va_list ap);
 
-static char *copy_wide_scan_format(const wchar_t *format)
+static size_t encode_wide_value(wchar_t value, int utf8, char bytes[4])
+{
+    mbstate_t state = {0U, 0U};
+
+    return __mini_wcrtomb_mode(bytes, value, &state, utf8);
+}
+
+static char *encode_wide_scan_format(const wchar_t *format, int utf8)
 {
     char *narrow;
     size_t length = 0U;
-    size_t i;
+    size_t index;
 
     if (format == (const wchar_t *)0) {
         errno = EINVAL;
         return (char *)0;
     }
-    while (format[length] != 0) {
-        if (format[length] < 0 || (unsigned int)format[length] > 0x7fU) {
-            errno = EILSEQ;
+
+    for (index = 0U; format[index] != 0; ++index) {
+        char bytes[4];
+        size_t converted = encode_wide_value(format[index], utf8, bytes);
+
+        if (converted == (size_t)-1) {
             return (char *)0;
         }
-        ++length;
+        if (length > (size_t)-1 - converted - 1U) {
+            errno = ENOMEM;
+            return (char *)0;
+        }
+        length += converted;
     }
 
     narrow = (char *)malloc(length + 1U);
     if (narrow == (char *)0) {
         return (char *)0;
     }
-    for (i = 0U; i < length; ++i) {
-        narrow[i] = (char)format[i];
+
+    length = 0U;
+    for (index = 0U; format[index] != 0; ++index) {
+        char bytes[4];
+        size_t converted = encode_wide_value(format[index], utf8, bytes);
+        size_t i;
+
+        if (converted == (size_t)-1) {
+            free(narrow);
+            return (char *)0;
+        }
+        for (i = 0U; i < converted; ++i) {
+            narrow[length++] = bytes[i];
+        }
     }
     narrow[length] = '\0';
     return narrow;
@@ -42,18 +70,21 @@ static char *copy_wide_scan_format(const wchar_t *format)
 
 int vfwscanf(FILE *restrict stream, const wchar_t *restrict format, va_list ap)
 {
-    char *narrow = copy_wide_scan_format(format);
+    char *narrow = (char *)0;
     int result;
-
-    if (narrow == (char *)0) {
-        return EOF;
-    }
 
     __mini_stdio_lock();
     if (__mini_stdio_require_wide(stream) == EOF) {
         result = EOF;
     } else {
-        result = __mini_vwscan_dispatch(stream, narrow, ap);
+        int utf8 = (stream->state & MINI_FILE_WIDE_UTF8) != 0U;
+
+        narrow = encode_wide_scan_format(format, utf8);
+        if (narrow == (char *)0) {
+            result = EOF;
+        } else {
+            result = __mini_vwscan_dispatch(stream, narrow, ap);
+        }
     }
     __mini_stdio_unlock();
     free(narrow);
@@ -90,14 +121,15 @@ int wscanf(const wchar_t *restrict format, ...)
 int vswscanf(const wchar_t *restrict input, const wchar_t *restrict format,
              va_list ap)
 {
-    char *narrow = copy_wide_scan_format(format);
+    char *narrow;
     int result;
 
     if (input == (const wchar_t *)0) {
-        free(narrow);
         errno = EINVAL;
         return EOF;
     }
+
+    narrow = encode_wide_scan_format(format, __mini_locale_is_utf8());
     if (narrow == (char *)0) {
         return EOF;
     }
