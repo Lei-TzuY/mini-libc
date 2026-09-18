1abc63c0c1c0f3f5e54873fce4335e3de876e6d8
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-18 14:50:26 +0800

    Add C.UTF-8 locale mode

--- src/locale/locale.c
+++ src/locale/locale.c
@@ -1,10 +1,15 @@
 #include <locale.h>
+#include <stddef.h>
+
+#include "locale_internal.h"
 
 #define MINI_CHAR_MAX 127
 
-static char mini_locale_name[] = "C";
+static char mini_locale_c[] = "C";
+static char mini_locale_utf8[] = "C.UTF-8";
 static char mini_decimal_point[] = ".";
 static char mini_empty[] = "";
+static int mini_ctype_utf8;
 
 static struct lconv mini_c_locale = {
     mini_decimal_point,
@@ -52,16 +57,48 @@ static int same_string(const char *left, const char *right)
     return *left == *right;
 }
 
+static int utf8_name(const char *locale)
+{
+    return same_string(locale, "C.UTF-8") || same_string(locale, "C.utf8");
+}
+
+int __mini_locale_is_utf8(void)
+{
+    return mini_ctype_utf8;
+}
+
+size_t __mini_mb_cur_max(void)
+{
+    return mini_ctype_utf8 ? 4U : 1U;
+}
+
 char *setlocale(int category, const char *locale)
 {
     if (!valid_category(category)) {
         return (char *)0;
     }
+
     if (locale == (const char *)0) {
-        return mini_locale_name;
+        if (category == LC_CTYPE || category == LC_ALL) {
+            return mini_ctype_utf8 ? mini_locale_utf8 : mini_locale_c;
+        }
+        return mini_locale_c;
     }
+
+    if (category == LC_CTYPE || category == LC_ALL) {
+        if (*locale == '\0' || same_string(locale, "C")) {
+            mini_ctype_utf8 = 0;
+            return mini_locale_c;
+        }
+        if (utf8_name(locale)) {
+            mini_ctype_utf8 = 1;
+            return mini_locale_utf8;
+        }
+        return (char *)0;
+    }
+
     if (*locale == '\0' || same_string(locale, "C")) {
-        return mini_locale_name;
+        return mini_locale_c;
     }
     return (char *)0;
 }
