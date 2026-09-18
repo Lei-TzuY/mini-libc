652728a740ad1430a96ae534682c7dee517aca5c
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-18 21:05:21 +0800

    Build Unicode property runtime

--- mk/locale.mk
+++ mk/locale.mk
@@ -7,8 +7,9 @@ MULTIBYTE_RENAMES := -Dmblen=mini_test_mblen -Dmbtowc=mini_test_mbtowc \
                      -Dwctomb=mini_test_wctomb -Dmbstowcs=mini_test_mbstowcs \
                      -Dwcstombs=mini_test_wcstombs
 
-$(LIBC): $(BUILD)/locale.o $(BUILD)/wchar.o $(BUILD)/wctype.o $(BUILD)/wide_stdio.o \
-         $(BUILD)/wide_format.o $(BUILD)/wide_scan.o $(BUILD)/multibyte.o
+$(LIBC): $(BUILD)/locale.o $(BUILD)/wchar.o $(BUILD)/unicode_props.o $(BUILD)/wctype.o \
+         $(BUILD)/wide_stdio.o $(BUILD)/wide_format.o $(BUILD)/wide_scan.o \
+         $(BUILD)/multibyte.o
 all: $(BUILD)/locale_probe $(BUILD)/locale_differential \
      $(BUILD)/wchar_probe $(BUILD)/wchar_differential \
      $(BUILD)/wide_stdio_probe
@@ -23,7 +24,10 @@ $(BUILD)/locale.o: src/locale/locale.c src/locale/locale_internal.h include/loca
 $(BUILD)/wchar.o: src/wchar/wchar.c src/wchar/wchar_internal.h src/locale/locale_internal.h include/wchar.h include/stddef.h include/errno.h | $(BUILD)
 	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@
 
-$(BUILD)/wctype.o: src/wchar/wctype.c include/wctype.h include/wchar.h include/ctype.h | $(BUILD)
+$(BUILD)/unicode_props.o: src/wchar/unicode_props.c src/wchar/unicode_props.h include/stddef.h | $(BUILD)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@
+
+$(BUILD)/wctype.o: src/wchar/wctype.c src/wchar/unicode_props.h src/locale/locale_internal.h include/wctype.h include/wchar.h include/ctype.h | $(BUILD)
 	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@
 
 $(BUILD)/wide_stdio.o: src/wchar/wide_stdio.c include/wchar.h include/stdio.h \
