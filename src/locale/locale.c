817a8c079f7ac64f3744ceef7aae31d34b8481ac
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-23 01:30:15 +0800

    feat: resolve locale selection from environment

--- src/locale/locale.c
+++ src/locale/locale.c
@@ -1,15 +1,25 @@
 #include <locale.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 #include "locale_internal.h"
 
 #define MINI_CHAR_MAX 127
 
+enum mini_locale_mode {
+    MINI_LOCALE_C = 0,
+    MINI_LOCALE_C_UTF8 = 1
+};
+
+struct mini_locale_state {
+    enum mini_locale_mode ctype;
+};
+
 static char mini_locale_c[] = "C";
 static char mini_locale_utf8[] = "C.UTF-8";
 static char mini_decimal_point[] = ".";
 static char mini_empty[] = "";
-static int mini_ctype_utf8;
+static struct mini_locale_state mini_process_locale = {MINI_LOCALE_C};
 
 static struct lconv mini_c_locale = {
     mini_decimal_point,
@@ -62,14 +72,93 @@ static int utf8_name(const char *locale)
     return same_string(locale, "C.UTF-8") || same_string(locale, "C.utf8");
 }
 
+static const char *category_environment_name(int category)
+{
+    switch (category) {
+    case LC_CTYPE:
+        return "LC_CTYPE";
+    case LC_NUMERIC:
+        return "LC_NUMERIC";
+    case LC_TIME:
+        return "LC_TIME";
+    case LC_COLLATE:
+        return "LC_COLLATE";
+    case LC_MONETARY:
+        return "LC_MONETARY";
+    default:
+        return (const char *)0;
+    }
+}
+
+static const char *nonempty_environment(const char *name)
+{
+    char *value = getenv(name);
+
+    if (value == (char *)0 || *value == '\0') {
+        return (const char *)0;
+    }
+    return value;
+}
+
+static const char *environment_locale(int category)
+{
+    const char *value;
+    const char *category_name;
+
+    value = nonempty_environment("LC_ALL");
+    if (value != (const char *)0) {
+        return value;
+    }
+
+    category_name = category_environment_name(category);
+    if (category_name != (const char *)0) {
+        value = nonempty_environment(category_name);
+        if (value != (const char *)0) {
+            return value;
+        }
+    }
+
+    value = nonempty_environment("LANG");
+    return value != (const char *)0 ? value : mini_locale_c;
+}
+
+static char *current_locale(int category)
+{
+    if ((category == LC_CTYPE || category == LC_ALL) &&
+        mini_process_locale.ctype == MINI_LOCALE_C_UTF8) {
+        return mini_locale_utf8;
+    }
+    return mini_locale_c;
+}
+
+static char *apply_locale(int category, const char *locale)
+{
+    if (category == LC_CTYPE || category == LC_ALL) {
+        if (same_string(locale, "C")) {
+            mini_process_locale.ctype = MINI_LOCALE_C;
+            return mini_locale_c;
+        }
+        if (utf8_name(locale)) {
+            mini_process_locale.ctype = MINI_LOCALE_C_UTF8;
+            return mini_locale_utf8;
+        }
+        return (char *)0;
+    }
+
+    if (same_string(locale, "C")) {
+        return mini_locale_c;
+    }
+    return (char *)0;
+}
+
 int __mini_locale_is_utf8(void)
 {
-    return mini_ctype_utf8;
+    return mini_process_locale.ctype == MINI_LOCALE_C_UTF8;
 }
 
 size_t __mini_mb_cur_max(void)
 {
-    return mini_ctype_utf8 ? 4U : 1U;
+    return __mini_locale_is_utf8() ? 4U : 1U;
 }
 
 char *setlocale(int category, const char *locale)
@@ -79,28 +168,14 @@ char *setlocale(int category, const char *locale)
     }
 
     if (locale == (const char *)0) {
-        if (category == LC_CTYPE || category == LC_ALL) {
-            return mini_ctype_utf8 ? mini_locale_utf8 : mini_locale_c;
-        }
-        return mini_locale_c;
+        return current_locale(category);
     }
 
-    if (category == LC_CTYPE || category == LC_ALL) {
-        if (*locale == '\0' || same_string(locale, "C")) {
-            mini_ctype_utf8 = 0;
-            return mini_locale_c;
-        }
-        if (utf8_name(locale)) {
-            mini_ctype_utf8 = 1;
-            return mini_locale_utf8;
-        }
-        return (char *)0;
+    if (*locale == '\0') {
+        locale = environment_locale(category);
     }
 
-    if (*locale == '\0' || same_string(locale, "C")) {
-        return mini_locale_c;
-    }
-    return (char *)0;
+    return apply_locale(category, locale);
 }
 
 struct lconv *localeconv(void)
