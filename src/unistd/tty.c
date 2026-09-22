53b45d90ed9f531a7ef3f3aafd2c97613728a983
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-23 06:32:16 +0800

    feat: add termios runtime

--- src/unistd/tty.c
+++ src/unistd/tty.c
@@ -2,6 +2,7 @@
 #include <mini/syscall.h>
 #include <mini/tty_ioctl.h>
 #include <sys/types.h>
+#include <termios.h>
 #include <unistd.h>
 
 pid_t tcgetpgrp(int fd)
@@ -26,6 +27,47 @@ int tcsetpgrp(int fd, pid_t pgrp)
     long result = mini_sys_ioctl(fd, MINI_TIOCSPGRP,
                                  (unsigned long)&pgid);
 
+    if (result < 0L) {
+        errno = (int)-result;
+        return -1;
+    }
+    errno = saved_errno;
+    return 0;
+}
+
+int tcgetattr(int fd, struct termios *termios_p)
+{
+    int saved_errno = errno;
+    long result = mini_sys_ioctl(fd, MINI_TCGETS,
+                                 (unsigned long)termios_p);
+
+    if (result < 0L) {
+        errno = (int)-result;
+        return -1;
+    }
+    errno = saved_errno;
+    return 0;
+}
+
+int tcsetattr(int fd, int optional_actions,
+              const struct termios *termios_p)
+{
+    unsigned long request;
+    int saved_errno = errno;
+    long result;
+
+    if (optional_actions == TCSANOW) {
+        request = MINI_TCSETS;
+    } else if (optional_actions == TCSADRAIN) {
+        request = MINI_TCSETSW;
+    } else if (optional_actions == TCSAFLUSH) {
+        request = MINI_TCSETSF;
+    } else {
+        errno = EINVAL;
+        return -1;
+    }
+
+    result = mini_sys_ioctl(fd, request, (unsigned long)termios_p);
     if (result < 0L) {
         errno = (int)-result;
         return -1;
