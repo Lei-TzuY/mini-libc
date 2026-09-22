72026b07276a283cd96dacf93a5750af47bbe27d
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-23 06:42:39 +0800

    feat: expose stopped child wait status

--- include/sys/wait.h
+++ include/sys/wait.h
@@ -4,12 +4,15 @@
 #include <sys/types.h>
 
 #define WNOHANG 1
+#define WUNTRACED 2
 
 #define WTERMSIG(status) ((status) & 0x7f)
 #define WIFEXITED(status) (WTERMSIG(status) == 0)
 #define WEXITSTATUS(status) (((status) >> 8) & 0xff)
+#define WSTOPSIG(status) WEXITSTATUS(status)
 #define WIFSIGNALED(status) \
     (WTERMSIG(status) != 0 && WTERMSIG(status) != 0x7f)
+#define WIFSTOPPED(status) (((status) & 0xff) == 0x7f)
 
 pid_t waitpid(pid_t pid, int *status, int options);
 
